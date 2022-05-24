34fbb95629de2a60a81518abad72955bfa4bf852
marco.cpd <marco.cpd@sec-educacao.local>
2022-05-24 16:01:53 -0300

    El código

---
+++ consultar_chamado.php
@@ -0,0 +1,96 @@
+<?php require_once "validador_acesso.php"; ?>
+
+<?php
+  
+  $arquivo = fopen('arquivo.hd', 'r');
+  $registros_chamados = array();
+
+  while(!feof($arquivo)){
+
+      $registro = fgets($arquivo);
+      $registros_chamados[] = $registro;
+  }
+
+  fclose($arquivo);
+
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+      .card-consultar-chamado {
+        padding: 30px 0 0 0;
+        width: 100%;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="Imagens/helpdesk-icon-13.jpg" width="40" height="40" class="d-inline-block align-top" alt="">
+        App Help Desk
+      </a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="logoff.php">SAIR</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-consultar-chamado">
+          <div class="card">
+            <div class="card-header">
+              Consulta de chamado
+            </div>
+            
+            <div class="card-body">
+              
+              <?php foreach ($registros_chamados as $registros ) { ?>
+
+                <?php
+
+                  
+                  $dados = explode('#', $registros);
+
+                  if($dados[0] == $_SESSION['id'] )
+                  {
+                      if(count($dados) < 3){
+                        continue;
+                      }
+                  }
+                 
+                  
+                ?>
+
+              <div class="card mb-3 bg-light">
+                <div class="card-body">
+                  <h5 class="card-title"><?=$dados[1]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?=$dados[2]?></h6>
+                  <p class="card-text"><?=$dados[3]?></p>
+
+                </div>
+              </div>
+            <?php } ?>
+              <div class="row mt-5">
+                <div class="col-6">
+                  <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ login_validade.php
@@ -0,0 +1,40 @@
+<?php
+    
+    //Inicindo sessão para validar acesso do usuário 
+    session_start();
+
+    //
+    $validar_user = false;
+    $usuario_id = null;
+
+    $list_user = [
+        ['id' => 1,'email' => 'marcojunior.reis@hotmail.com', 'senha' => '12345', 'perfil_id' => 1],
+        ['id' => 2, 'email' => 'marco.reis@aluno.ifsp.edu.br', 'senha' => '12345', 'perfil_id' => 2]
+    ];
+
+
+    
+    foreach($list_user as $user){   
+
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha'] ){
+                    $validar_user = true;
+                    $usuario_id = $user['id'];
+                    $perfil_id = $user['perfil_id'];
+            }
+    }
+        
+    
+
+    if($validar_user){
+        //echo'Usuário autenticado';
+        $_SESSION['autenticado']= 'SIM';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil'] = $perfil_id;
+        header('Location: home.php');
+    }else{
+        header('Location: index.php?login=erro');
+        $_SESSION['autenticado']= 'NAO';
+    }   
+
+        
+?>
